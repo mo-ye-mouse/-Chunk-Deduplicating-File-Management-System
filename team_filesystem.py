d886ac71dfcfdf55dff2e6a6dfded68e32062d30
黄彦达 <2407859806@qq.com>
2025-06-06 18:08:12 +0800

    完成了rename和delete_item的函数部分

--- team_filesystem.py
+++ team_filesystem.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import time
+from os import remove
 
 
 class FileManager:
@@ -38,9 +39,36 @@ class FileManager:
 
     def rename(self, *args):
         """处理rename命令"""
+        if len(args) != 2:
+            print("格式错误，用法: rename 旧名 新名")
+            return
+        old_name,new_name=args
+        old_path=os.path.join(self.current_dir,old_name)
+        new_path=os.path.join(self.current_dir,new_name)
+        if not os.path.exists(old_path):
+            print(f"错误：{old_name}不存在")
+            return
+        try:
+            os.rename(old_path,new_path)
+            print("重命名成功")
+        except Exception as e:
+            print(f"重命名失败：{e}")
 
     def delete_item(self, *args):
         """处理del/rm命令"""
+        if len(args) != 1:
+            print("格式错误，用法：del 单个文件的文件名")
+            return
+        file_name=args[0]
+        file_path=os.path.join(self.current_dir,file_name)
+        if not os.path.isfile(file_path):
+            print(f"错误：{file_path}不是单个文件或不存在")
+            return
+        try:
+            os.remove(file_path)
+            print("成功删除该文件")
+        except Exception as e:
+            print(f"删除文件失败：{e}")
 
     def delete_directory(self, *args):
         """处理rmdir/rd命令"""
