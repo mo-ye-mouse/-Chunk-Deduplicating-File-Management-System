8843ee61daed40a8e7d169627d3e93c3e3027d80
bzm <3234001587@qq.com>
2025-11-14 20:50:32 +0800

    1

--- 消解算法.py
+++ 消解算法.py
@@ -2,13 +2,13 @@ import re
 #消除合式公式中的等价式
 def eliminate_operate1(expr):
     while '↔' in expr:
-        expr = re.sub(r'([A-Za-z¬∨∧()]+)→([A-Za-z¬∨∧()]+)',r'(\1→\2)∧(\2→\1)', expr, count=1)
+        expr = re.sub(r'([A-Za-z¬∨∧()]+)↔([A-Za-z¬∨∧()]+)',r'(\1→\2)∧(\2→\1)', expr, count=1)
     return expr
 
 #消除合式公式中的蕴含式
 def eliminate_operate2(expr):
     while '→' in expr:
-        expr = re.sub(r'([A-Za-z¬∨∧()]+)↔([A-Za-z¬∨∧()]+)',r'(¬\1∨\2)', expr, count=1)
+        expr = re.sub(r'([A-Za-z¬∨∧()]+)→([A-Za-z¬∨∧()]+)',r'(¬\1∨\2)', expr, count=1)
     return expr
 
 #将合式公式转化成合取范式
@@ -22,6 +22,8 @@ def transform(expr):
     #析取对合取的分配律
 
     return expr
+expr=input()
+print(transform(expr))
 
 
 
